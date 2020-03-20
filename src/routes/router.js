1490c7bd22bf3d4d585acc76df5d10b13891c52a
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-19 22:32:16 -0600

    regular and irregular verbs routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,7 +6,13 @@ module.exports = function(){
     router.get("/", controllers.Home);
 
     // Mostrar Frases
-    router.get("/phrases", controllers.GetPhrases);
+    router.get("/get-phrases", controllers.GetPhrasal);
+
+    // Mostrar Verbos regulares
+    router.get("/regular-verbs", controllers.RegularVerbs);
+
+    // Mostrar Verbos irregulares
+    router.get("/irregular-verbs", controllers.IrregularVerbs);
 
     // //Agregar Vehiculos
     // router.post("/add-vehicles", controllers.addVehicles);
