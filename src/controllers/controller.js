d3f56af8fe5c696a0a3cefc229359a3a079361a0
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-19 22:33:03 -0600

    regular and irregular verbs controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,15 +1,43 @@
-const Phrases = require("../models/phrases");
+const Phrases = require("../models/getPhrasal");
+const RegularVerbs = require("../models/regularVerbs");
+const IrregularVerbs = require("../models/irregularVerbs");
 
 //Principal
 exports.Home = (req,res)=>{
     res.send('Made by Jesus Ponce');
 };
 
-exports.GetPhrases = async (req,res)=>{
+exports.GetPhrasal = async (req,res)=>{
     try {
-        const PhrasesAll = await Phrases.findAll();
+        const GetPhrasesAll = await Phrases.findAll();
         res.json({
-            "phrases" : PhrasesAll});
+            "GetPhrases" : GetPhrasesAll});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: "Error en el servidor "
+          });
+    }
+}
+
+exports.RegularVerbs = async (req,res)=>{
+    try {
+        const RegularVerbsAll = await RegularVerbs.findAll();
+        res.json({
+            "RegularVerbs" : RegularVerbsAll});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: "Error en el servidor "
+          });
+    }
+}
+
+exports.IrregularVerbs = async (req,res)=>{
+    try {
+        const IrregularVerbsAll = await IrregularVerbs.findAll();
+        res.json({
+            "IrregularVerbs" : IrregularVerbsAll});
     } catch (e) {
         console.log(e);
         res.status(500).json({
